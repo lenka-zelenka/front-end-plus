443bd53111ff3f82a7f891b934fa3e22f5e5924f
lenka-zelenka <elenazologina@gmail.com>
2018-06-30 18:11:33 +0300

    tru render data

--- lections/lection_02_Async JS/homework/modules/renderService.js
+++ lections/lection_02_Async JS/homework/modules/renderService.js
@@ -1,10 +1,5 @@
-var organization = [],
-    repos = [];
-
 
 function renderData(data) {
-    organization = data[0];
-    repos = data[1];
     console.log(data)
     // var logo = document.getElementById("logo");
     // logo.innerHTML += `<img src="${organization.avatar_url}" alt="">`;  
