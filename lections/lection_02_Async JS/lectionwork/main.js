9346521e8ce1090ab1ba23440f1e53ef3a79f351
lenka-zelenka <elenazologina@gmail.com>
2018-07-07 23:25:08 +0300

    try to add branch updates

--- lections/lection_02_Async JS/lectionwork/main.js
+++ lections/lection_02_Async JS/lectionwork/main.js
@@ -2,14 +2,14 @@ function load(method, path) {
     var xhr = new XMLHttpRequest();
     xhr.open(metod, path, true)
     xhr.send();
-    return new Promise(resolve, reject) {
+    return new Promise((resolve, reject) => {
         xhr.onreadystatechange = function () {
             if (xhr.readyState != 4) {
                 return;
             }
             resolve(JSON.parse(xhr.responseText));
         }
-    };
+    })
 }
 
 
