747326f3afeca8c7bc0575bbd595e1b494752c68
lenka-zelenka <elenazologina@gmail.com>
2018-08-05 12:05:36 +0300

    currensy table

--- lections/lection_06_Modules/homework/main.js
+++ lections/lection_06_Modules/homework/main.js
@@ -1,11 +1,29 @@
-fetch('https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2014')
-  .then(function(response) {
-    alert(response.headers.get('Content-Type')); // application/json; charset=utf-8
-    alert(response.status); // 200
 
-    return response.json();
-   })
-  .then(function(exchangeRate) {
-    alert(exchangeRate.saleRateNB); 
-  })
-  .catch( alert );
+// var apiKey = '912e5fbedf66b8fd805063c67d6078a1';
+var date = '01.12.2016';
+var url = 'https://api.privatbank.ua/p24api/exchange_rates?json&date='+ date;
+var respData; 
+
+
+fetch(url)
+    .then(res => res.json())
+    .then(resp => respData = resp )
+
+
+var render = function(respData){
+    var date = respData.date;
+    return `<table>
+    <tr>
+        <th>${date}</th>
+        <th>2</th>
+        <th>3</th>
+    </tr>
+    <tbody>
+        <tr>
+            <td>sewe</td>
+            <td>wewer</td>
+            <td>werwer</td>
+        </tr>
+    </tbody>
+</table>`
+}(respData)
