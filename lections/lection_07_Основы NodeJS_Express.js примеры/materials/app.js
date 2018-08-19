9549ddab34e2910cebb49b461e7692afaf7bf9a9
lenka-zelenka <elenazologina@gmail.com>
2018-08-19 11:49:23 +0300

    l7

--- lections/lection_07_Основы NodeJS_Express.js примеры/materials/app.js
+++ lections/lection_07_Основы NodeJS_Express.js примеры/materials/app.js
@@ -1,16 +1,30 @@
 let express = require('express');
+let fs = require('fs');
 let app = express();
 let port = 3780;
 
-
+let access = true;
 
 // MEAN - MongoDb, Express.js, Angular, Node.js 
-console.log(1);
+// console.log(1);
+
+app.use(function ( req, res, next) {
+    if (!access) {
+        return next(new Error());
+    }
+    return next();
+});
+
+app.use('/hack', function (req, res, next) {
+    console.log('hack');
+    access = false;
+    next(new Error());
+});
 
 app.use(function (req, res, next) {
-  console.log('-----------------------');
-  console.log('Time:', Date.now());
-  next();
+    console.log('-----------------------');
+    console.log('Time:', Date.now());
+    next();
 });
 
 app.use(function (req, res, next) {
@@ -20,7 +34,7 @@ app.use(function (req, res, next) {
 
 app.get('/foo', function (req, res, next) {
     console.log('Request Type:', req.method);
-    
+
     // res.send('hello /foo');
     next();
 });
@@ -32,11 +46,20 @@ app.get('/foo', function (req, res, next) {
     next();
 });
 
-app.use(function (req, res, next) {
-    console.log('next');
 
+
+app.use(function (req, res, next) {
+    var now = new Date();
+    var hour = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    var data = `${hour}:${minutes}:${seconds} ${req.method} ${req.url} ${req.get("user-agent")} \n`;
+
+    fs.appendFileSync("log.txt", data);
     next();
 });
+
+
 
 app.listen(port);
 
