592693adfe73b065a64205d13c0dc442a4fb25c6
lenka-zelenka <elenazologina@gmail.com>
2018-09-16 11:56:19 +0300

    start new vue project

---
+++ lections/lection_10_vuejs_cli/homework/lectionwork/my-project/src/components/LoginForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <label>Login</label>
+    <input type="text" >
+    <button v-on:click="checkValue">Login</button>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
