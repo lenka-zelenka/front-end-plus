dac4b6318bc979f377834b9d8f8b2fb8d7c8a2cd
lenka-zelenka <elenazologina@gmail.com>
2018-11-11 09:50:43 +0200

    commit prev smth

---
+++ lections/lection_12/lection_work/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Banner 
+      v-bind:type="'desktop'"
+      v-bind:bg="{url: {desktop: 'http://www.fresher.ru/wp-content/uploads/2018/03/1.jpg'}}"
+      v-bind:title="'Card Title'"
+      v-bind:price="444"
+      v-bind:btn-title="'Купить сразу!'"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Banner from './components/Banner.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Banner
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
